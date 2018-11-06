665a0001d36713b13638dae689a818b5cbcc88d7
zackhitch <zmhitchcock+git@gmail.com>
2018-11-06 13:29:05 -0800

    added dbConfig, modified usersModel, usersRoutes

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -1,44 +1,44 @@
-// const db = require('../../data/dbConfig.js');
+const db = require('../../data/dbConfig.js');
 
-// function find() {
-//   return db('users');
-// }
+function find() {
+  return db('users');
+}
 
-// function findByUserId(id) {
-//   return db('users')
-//     .where({ id })
-//     .first();
-// }
+function findByUserId(id) {
+  return db('users')
+    .where({ id })
+    .first();
+}
 
-// function findByEmail(email_address) {
-//   return db('users')
-//     .where({ email_address })
-//     .first();
-// }
+function findByEmail(email) {
+  return db('users')
+    .where({ email })
+    .first();
+}
 
-// function addUser(user) {
-//   return db('users')
-//     .insert(user)
-//     .into('users');
-// }
+function addUser(user) {
+  return db('users')
+    .insert(user)
+    .into('users');
+}
 
-// function updateUser(id, changes) {
-//   return db('users')
-//     .where({ id })
-//     .update(changes);
-// }
+function updateUser(id, changes) {
+  return db('users')
+    .where({ id })
+    .update(changes);
+}
 
-// function removeUser(id) {
-//   return db('users')
-//     .where({ id })
-//     .del();
-// }
+function removeUser(id) {
+  return db('users')
+    .where({ id })
+    .del();
+}
 
-// module.exports = {
-//   find,
-//   findByUserId,
-//   findByEmail,
-//   addUser,
-//   updateUser,
-//   removeUser,
-// };
+module.exports = {
+  find,
+  findByUserId,
+  findByEmail,
+  addUser,
+  updateUser,
+  removeUser,
+};
