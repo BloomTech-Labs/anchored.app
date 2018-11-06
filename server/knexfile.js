c4bdabf207d3454eb4c5c2e3aabbd36c04fd3d41
crypto-jones <integral.being@gmail.com>
2018-11-06 10:46:59 -0800

    Configure knex for development

---
+++ server/knexfile.js
@@ -0,0 +1,49 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/chainpoint_docusign.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  // **** CONFIGURE FOR PRODUCTION ***
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+};
