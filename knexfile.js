8ebc3473d38097d5c14852a36b8c117713e52ef8
crypto-jones <integral.being@gmail.com>
2018-11-06 15:12:22 -0800

    Update knex config for production

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+
+const localPg = {
+  host: 'localhost',
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -12,38 +23,19 @@ module.exports = {
       directory: './data/seeds',
     },
   },
-
-  // **** CONFIGURE FOR PRODUCTION ***
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'pg', // npm add pg
+    connection: dbConnection, // could be an object or a string
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
 };
