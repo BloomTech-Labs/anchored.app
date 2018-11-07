49523c064e2cb092c3c9f8cee976ff34e94fe867
DirupT <iamcooled@gmail.com>
2018-11-07 17:06:03 -0600

    added auth routes to server

---
+++ client/src/components/Auth/Docusign/DocusignLogin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Docusign extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="http://localhost:9000/auth">Docusign Login</a>
+      </div>
+    );
+  }
+}
+
+export default Docusign;
