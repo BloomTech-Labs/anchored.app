6043fc375d88be0f40bd11a8069dfadb3f4ae6bb
DirupT <iamcooled@gmail.com>
2018-11-13 20:56:31 -0600

    updates middleware to check docusign access_token and uses refresh_token to get a new one if it has expired

--- server/api/auth/auth0/auth0Middleware.js
+++ server/api/auth/auth0/auth0Middleware.js
@@ -18,6 +18,7 @@ async function userPostCheck(user) {
   new_user = {
     id,
     document_expiration: 0,
+    token_expiration: 0,
     first_name: given_name,
     last_name: family_name,
     username: nickname,
---
+++ server/api/documents/docsMiddleware.js
@@ -0,0 +1,53 @@
+const moment = require('moment');
+const axios = require('axios');
+const users = require('../users/usersModel');
+
+function handleExpiration(req) {
+  let header = {
+    headers: { Authorization: 'Basic ' + process.env.DOCUSIGN_BASE64 },
+  };
+
+  let data = {
+    grant_type: 'refresh_token',
+    refresh_token: req.user.refresh_token,
+  };
+
+  axios
+    .post('https://account-d.docusign.com/oauth/token', data, header)
+    .then(res => {
+      let expiration = moment().add(res.data.expires_in, 's');
+      req.user.access_token = res.data.access_token;
+      req.user.refresh_token = res.data.refresh_token;
+      req.user.token_expiration = JSON.stringify(expiration);
+      req.session.save();
+      users.updateUser(req.user.id, req.user).catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+}
+
+function checkExpiration(req, res, next) {
+  let current_time = new Date().getTime();
+
+  if (current_time > Number(req.user.document_expiration)) {
+    req.user.document_expiration = new Date().getTime() + 15 * 60 * 1000;
+    req.session.save();
+    users.updateUser(req.user.id, req.user).catch(err => console.log(err));
+    return next();
+  }
+  return res.status(400).json({ error: '15 minutes not up' });
+}
+
+function checkToken(req, res, next) {
+  let expiration = JSON.parse(req.user.token_expiration);
+  let now = moment();
+
+  if (req.user.access_token && now.add(30, 'm').isBefore(expiration)) {
+    return next();
+  } else if (!req.user.access_token || !req.user.refresh_token) {
+    return res.status(401).json({ message: 'You need to be logged in!' });
+  } else {
+    handleExpiration(req);
+  }
+}
+
+module.exports = { checkExpiration, checkToken };
