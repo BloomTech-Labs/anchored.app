60963bd21eadc6445f33534fbeed2d4980e3f2ac
DirupT <iamcooled@gmail.com>
2018-11-13 22:22:29 -0600

    added solution to an invalid / expired refresh token

--- server/api/documents/docsMiddleware.js
+++ server/api/documents/docsMiddleware.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const axios = require('axios');
 const users = require('../users/usersModel');
 
-function handleExpiration(req) {
+function handleExpiration(req, res, next) {
   let header = {
     headers: { Authorization: 'Basic ' + process.env.DOCUSIGN_BASE64 },
   };
@@ -14,15 +14,16 @@ function handleExpiration(req) {
 
   axios
     .post('https://account-d.docusign.com/oauth/token', data, header)
-    .then(res => {
-      let expiration = moment().add(res.data.expires_in, 's');
-      req.user.access_token = res.data.access_token;
-      req.user.refresh_token = res.data.refresh_token;
+    .then(response => {
+      let expiration = moment().add(response.data.expires_in, 's');
+      req.user.access_token = response.data.access_token;
+      req.user.refresh_token = response.data.refresh_token;
       req.user.token_expiration = JSON.stringify(expiration);
       req.session.save();
       users.updateUser(req.user.id, req.user).catch(err => console.log(err));
+      next();
     })
-    .catch(err => console.log(err));
+    .catch(err => res.status(401).json({ ErrorMessage: err.response.data }));
 }
 
 function checkExpiration(req, res, next) {
@@ -39,12 +40,12 @@ function checkToken(req, res, next) {
   let expiration = JSON.parse(req.user.token_expiration);
   let now = moment();
 
-  if (req.user.access_token && now.add(30, 'm').isBefore(expiration)) {
+  if (req.user.access_token && now.add(100000, 'm').isBefore(expiration)) {
     return next();
   } else if (!req.user.access_token || !req.user.refresh_token) {
     return res.status(401).json({ message: 'You need to be logged in!' });
   } else {
-    return handleExpiration(req);
+    return handleExpiration(req, res, next);
   }
 }
 
