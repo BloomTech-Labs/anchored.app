b6370f631178cb05191dd7de93cc6b7638080e83
Randy Calderon <randycweb@gmail.com>
2018-11-27 09:34:04 -0800

    Changes to image name

--- client/src/components/Stripe/Checkout.js
+++ client/src/components/Stripe/Checkout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
-import logo from '../../images/rsz_proofd-logo-resize.png';
+import logo from '../../images/proofdlogocheckout.png';
 import STRIPE_PUBLISHABLE from './constants/stripe';
 import PAYMENT_SERVER_URL from './constants/server';
 
