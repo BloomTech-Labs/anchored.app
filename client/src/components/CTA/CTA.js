57a3eb6b521f4b5330f873da2b5a5a993bb45954
zackhitch <zmhitchcock+git@gmail.com>
2018-11-28 13:53:48 -0800

    added react-scroll dependency, completed smooth scroll

--- client/src/components/CTA/CTA.js
+++ client/src/components/CTA/CTA.js
@@ -14,13 +14,46 @@ import {
   Friends,
 } from './styles/CTAStyles.js';
 
+import {
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from 'react-scroll';
+
 class CTA extends Component {
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function(to, element) {
+      console.log('begin', arguments);
+    });
+
+    Events.scrollEvent.register('end', function(to, element) {
+      console.log('end', arguments);
+    });
+
+    scrollSpy.update();
+  }
+
+  // scrolls to <Element name="pricingSheets"> in LPContent.js
+  smoothScroll() {
+    scroller.scrollTo('pricingSheets', {
+      duration: 1500,
+      delay: 1,
+      smooth: true,
+    });
+  }
+
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin');
+    Events.scrollEvent.remove('end');
+  }
+
   render() {
     return (
       <CtaContainer>
         <H2>Get proof, not just trust</H2>
         <Copy>Blockchain enabled verification platform</Copy>
-        <CtaButton>CHECK OUR PRICES</CtaButton>
+        <CtaButton onClick={this.smoothScroll}>CHECK OUR PRICES</CtaButton>
         <Demo>
           <Img src={playImg} alt="play icon" width="20%" />
           <Copy>Watch the Demo</Copy>
--- client/src/components/LPcontent/LPcontent.js
+++ client/src/components/LPcontent/LPcontent.js
@@ -12,6 +12,8 @@ import {
   PlayImg,
 } from './styles/LPcontentStyles.js';
 
+import { Element } from 'react-scroll';
+
 import ContentBox from '../ContentBox/ContentBox.js';
 import playImg from '../../assets/play-circle.svg';
 
@@ -63,20 +65,22 @@ const LPcontent = () => {
         <TagLine>The science behind our work</TagLine>
       </ContentSet>
       <H1>Killer Prices, Subscribe and Save</H1>
-      <ContentSet>
-        <ContentBox
-          title="Free Trial"
-          copyOne="3 Free Credits"
-          copyTwo="Purchase Additional Credits"
-          copyThree="Upgrade Anytime"
-        />
-        <ContentBox
-          title="Premium Subscription"
-          copyOne="20 Credits for:"
-          copyTwo="$15 charged per month or"
-          copyThree="$150 charged per year"
-        />
-      </ContentSet>
+      <Element name="pricingSheets">
+        <ContentSet>
+          <ContentBox
+            title="Free Trial"
+            copyOne="3 Free Credits"
+            copyTwo="Purchase Additional Credits"
+            copyThree="Upgrade Anytime"
+          />
+          <ContentBox
+            title="Premium Subscription"
+            copyOne="20 Credits for:"
+            copyTwo="$15 charged per month or"
+            copyThree="$150 charged per year"
+          />
+        </ContentSet>
+      </Element>
     </ContentMain>
   );
 };
