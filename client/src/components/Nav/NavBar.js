3478e10ec3a20af70ad82ddbc20075092bd5cbc7
crypto-jones <integral.being@gmail.com>
2018-11-28 10:00:53 -0800

    Refactor Nav styles

--- client/src/components/Nav/NavBar.js
+++ client/src/components/Nav/NavBar.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
-import styled from 'styled-components';
-
 import Auth0 from '../Auth/Auth0/Auth0.js';
 import NavButton from './NavButton.js';
+import { NavGod, TopNavBar, Links } from './styles/NavStyles.js';
 
 const auth = new Auth0();
-
-const NavGod = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const TopNavBar = styled.div`
-  max-width: 1026px;
-  width: 100%;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Links = styled.div`
-  width: 400px;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-`;
 
 class NavBar extends Component {
   render() {
