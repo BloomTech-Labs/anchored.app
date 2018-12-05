f7f1ce08950fdf45b107eb983beb13119bc7d4d1
DirupT <jjaguirre73@yahoo.com>
2018-12-05 10:43:26 -0600

    remove un-needed code

--- server/api/documents/docsMiddleware.js
+++ server/api/documents/docsMiddleware.js
@@ -2,10 +2,7 @@ const docs = require('./documentsModel');
 const { promisify } = require('util');
 
 async function postDoctoDB(req, res, documents) {
-  const ids = await docs.addDoc({
-    document: documents.documents,
-    envelope_id: documents.envelope_id,
-  });
+  const ids = await docs.addDoc({ document: documents.documents });
   const user_doc = { account_id: req.user.account_id, document_id: ids.id };
   await docs.addUserToDoc(user_doc);
 }
--- server/api/documents/documentsRoutes.js
+++ server/api/documents/documentsRoutes.js
@@ -1,15 +1,9 @@
 const express = require('express');
 
-const docs = require('./documentsModel');
-
 const { ensureAuthenticated } = require('../auth/docusign/dsMiddleware');
 
 const router = express.Router();
 
 router.use(ensureAuthenticated);
-
-router.get('/:id', (req, res) => {
-  docs.f;
-});
 
 module.exports = router;
