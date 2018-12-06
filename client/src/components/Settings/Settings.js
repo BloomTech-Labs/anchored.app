9433d22b4e4945c49a37a1a8aff46fd7fc7f2c57
crypto-jones <integral.being@gmail.com>
2018-12-06 13:22:01 -0800

    Create header and subheaders

--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import DocusignUnlink from '../Auth/Docusign/DocusignUnlink.js';
 import PasswordReset from './PasswordReset.js';
-import { ButtonContainer } from './styles/SettingsStyles.js';
+import {
+  ButtonContainer,
+  SettingsWrapper,
+  MainHeader,
+  SubHeader,
+} from './styles/SettingsStyles.js';
 
-const Settings = () => {
-  return (
-    <ButtonContainer>
-      <PasswordReset />
-      <DocusignUnlink />
-    </ButtonContainer>
-  );
+class Settings extends Component {
+  render() {
+    console.log(this.props.user);
+    return (
+      // <ButtonContainer>
+      //   <PasswordReset />
+      //   <DocusignUnlink />
+      // </ButtonContainer>
+      <SettingsWrapper>
+        <MainHeader>Settings</MainHeader>
+        <SubHeader>Email</SubHeader>
+        {this.props.user.email}
+      </SettingsWrapper>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    // User Info Data
+    user: state.user.user,
+  };
 };
 
-export default Settings;
+export default connect(mapStateToProps)(Settings);
--- client/src/components/Settings/styles/SettingsStyles.js
+++ client/src/components/Settings/styles/SettingsStyles.js
@@ -8,3 +8,24 @@ export const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
 `;
+
+export const SettingsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 30px;
+  max-width: 1010px;
+  height: 100%;
+`;
+
+export const MainHeader = styled.h3`
+  display: flex;
+  margin-bottom: 50px;
+`;
+
+export const SubHeader = styled.h4`
+  display: flex;
+  margin-bottom: 10px;
+  color: gray;
+`;
