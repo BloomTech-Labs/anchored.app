3986356796ffa0f2c51148f5e2bf7a996a126bdb
Randy Calderon <randycweb@gmail.com>
2018-12-11 09:48:02 -0800

    Revert "Removed Unused Actions/Reducers from Billing Components and Stripe"

--- client/src/actions/billing.js
+++ client/src/actions/billing.js
@@ -2,10 +2,52 @@ import axios from 'axios';
 
 import PAYMENT_SERVER_URL from '../components/Stripe/constants/server';
 
+export const RETRIEVING_ALL_USER_INFO = 'RETRIEVING_ALL_USER_INFO';
+export const RETRIEVED_ALL_USER_INFO = 'RETRIEVED_ALL_USER_INFO';
+
+export const RETRIEVING_INVOICE = 'RETRIEVING_INVOICE';
+export const RETRIEVED_INVOICE = 'RETRIEVED_INVOICE';
+
 export const RETRIEVING_CREDIT = 'RETRIEVING_CREDIT';
 export const RETRIEVED_CREDIT = 'RETRIEVED_CREDIT';
 
 export const ERROR = 'ERROR';
+
+export const getAllUserInfo = () => {
+  const promise = axios.get(
+    process.env.REACT_APP_USERS_INFO ||
+      'http://localhost:9000/users/subscription'
+  );
+  return dispatch => {
+    dispatch({ type: RETRIEVING_ALL_USER_INFO });
+    promise
+      .then(res =>
+        dispatch({
+          type: RETRIEVED_ALL_USER_INFO,
+          payload: res.data,
+        })
+      )
+      .catch(err => dispatch({ type: ERROR, payload: err.message }));
+  };
+};
+
+export const getUserInvoice = () => {
+  const promise = axios.get(
+    process.env.REACT_APP_USERS_INVOICE || 'http://localhost:9000/payment/'
+  );
+
+  return dispatch => {
+    dispatch({ type: RETRIEVING_INVOICE });
+    promise
+      .then(res =>
+        dispatch({
+          type: RETRIEVED_INVOICE,
+          payload: res.data,
+        })
+      )
+      .catch(err => dispatch({ type: ERROR, payload: err.message }));
+  };
+};
 
 const errorPayment = () => {
   alert(
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import user from './user';
 import envelopes from './envelopes';
+import billing from './billing';
 
 const rootReducer = combineReducers({
   user,
   envelopes,
+  billing,
 });
 
 export default rootReducer;
