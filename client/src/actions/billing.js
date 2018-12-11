1545f5e272896d9955250eac3c698e5849c504aa
Randy Calderon <randycweb@gmail.com>
2018-12-11 01:34:34 -0800

    Changes to billing actions

---
+++ client/src/actions/billing.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+import PAYMENT_SERVER_URL from '../components/Stripe/constants/server';
+
+export const RETRIEVING_CREDIT = 'RETRIEVING_CREDIT';
+export const RETRIEVED_CREDIT = 'RETRIEVED_CREDIT';
+
+export const ERROR = 'ERROR';
+
+const errorPayment = () => {
+  alert(
+    'There was an error processing your payment. Please contact customer support.'
+  );
+};
+
+export const onToken = (amount, description) => dispatch => token => {
+  const fromDollarToCent = amount => amount * 100;
+  const CURRENCY = 'USD';
+  const promise = axios.post(PAYMENT_SERVER_URL, {
+    description,
+    source: token.id,
+    currency: CURRENCY,
+    amount: fromDollarToCent(amount),
+  });
+
+  dispatch({ type: RETRIEVING_CREDIT });
+  promise
+    .then(() => {
+      let credit_add = description;
+      let credit_split = credit_add.split('');
+      let filterCredits = credit_split.filter(numStr => {
+        return /^[1,3,5]/.test(numStr);
+      });
+      dispatch({ type: RETRIEVED_CREDIT, payload: Number(filterCredits) });
+    })
+    .catch(() => {
+      errorPayment();
+      dispatch({ type: ERROR });
+    });
+};
