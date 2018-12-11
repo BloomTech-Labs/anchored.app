d03a28491c5853933be8068ec563e5b74938e6ec
Sean Chavez <realseanchavez@gmail.com>
2018-12-11 13:21:59 -0800

    add OurTeam page

---
+++ client/src/components/OurTeam/OurTeam.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OurTeam = () => {
+  return (
+    <div>
+      <h1>Our Team: </h1>
+      <p>Brandon Aguirre</p>
+      <p>Jeremy Jones</p>
+      <p>Zack Hitchcock</p>
+      <p>Randy Calderon</p>
+      <p>Sean Chavez</p>
+    </div>
+  );
+};
+
+export default OurTeam;
