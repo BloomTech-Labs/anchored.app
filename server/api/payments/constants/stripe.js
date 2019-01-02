c97eb86a6a1d2d1258d3146651722e64c583fb5d
zackhitch <zmhitchcock+git@gmail.com>
2019-01-02 11:12:36 -0800

    stripe change on backend

--- server/api/payments/constants/stripe.js
+++ server/api/payments/constants/stripe.js
@@ -1,9 +1,11 @@
 const configureStripe = require('stripe');
 
-const STRIPE_SECRET_KEY =
-  process.env.NODE_ENV === 'production'
-    ? process.env.STRIPE_LIVE_KEY
-    : process.env.STRIPE_TEST_KEY;
+const STRIPE_SECRET_KEY = process.env.STRIPE_TEST_KEY;
+
+// const STRIPE_SECRET_KEY =
+//   process.env.NODE_ENV === 'production'
+//     ? process.env.STRIPE_LIVE_KEY
+//     : process.env.STRIPE_TEST_KEY;
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
