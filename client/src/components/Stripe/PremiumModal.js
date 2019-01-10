13e8d02b1457e38bf138b34710c4d0f3aa48f4b5
crypto-jones <integral.being@gmail.com>
2019-01-09 20:10:55 -0800

    Add state and function for form

--- client/src/components/Stripe/PremiumModal.js
+++ client/src/components/Stripe/PremiumModal.js
@@ -9,6 +9,12 @@ import {
   Input,
 } from 'reactstrap';
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
+
 class PremiumModal extends Component {
   state = {
     name: '',
@@ -16,25 +22,38 @@ class PremiumModal extends Component {
   };
 
   handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error));
+
     e.preventDefault();
   };
 
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { name, email } = this.state;
+
     return (
       <Modal centered isOpen={this.props.isOpen} toggle={this.props.toggle}>
-        {/* <ModalBody>Sign up</ModalBody> */}
         <ModalHeader toggle={this.props.toggle}>
           Give us your email address, and we'll notify you when our premium
           service is available.
         </ModalHeader>
         <ModalBody>
           <Form>
-            <FormGroup>
+            <FormGroup onSubmit={this.handleSubmit}>
               <Input
-                type="name"
+                type="text"
                 name="name"
                 id="name"
                 placeholder="Full Name"
+                value={name}
+                onChange={this.handleChange}
               />
             </FormGroup>
             <FormGroup>
@@ -43,10 +62,18 @@ class PremiumModal extends Component {
                 name="email"
                 id="email"
                 placeholder="name@example.com"
+                value={email}
+                onChange={this.handleChange}
               />
             </FormGroup>
           </Form>
-          <Button size="lg" block color="info" style={{ marginTop: '20px' }}>
+          <Button
+            size="lg"
+            block
+            color="info"
+            style={{ marginTop: '20px' }}
+            onSubmit={this.handleSubmit}
+          >
             Sign Up
           </Button>
         </ModalBody>
