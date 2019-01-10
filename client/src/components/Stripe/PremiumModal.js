caef117eb77d6003f14f10ed3037419560da202a
crypto-jones <integral.being@gmail.com>
2019-01-09 19:52:02 -0800

    Refactor PremiumModal to class component

--- client/src/components/Stripe/PremiumModal.js
+++ client/src/components/Stripe/PremiumModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Button,
   Modal,
@@ -9,34 +9,50 @@ import {
   Input,
 } from 'reactstrap';
 
-const PremiumModal = ({ isOpen, toggle }) => {
-  return (
-    <Modal centered isOpen={isOpen} toggle={toggle}>
-      {/* <ModalBody>Sign up</ModalBody> */}
-      <ModalHeader toggle={toggle}>
-        Give us your email address, and we'll notify you when our premium
-        service is available.
-      </ModalHeader>
-      <ModalBody>
-        <Form>
-          <FormGroup>
-            <Input type="name" name="name" id="name" placeholder="Full Name" />
-          </FormGroup>
-          <FormGroup>
-            <Input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="name@example.com"
-            />
-          </FormGroup>
-        </Form>
-        <Button size="lg" block color="info" style={{ marginTop: '20px' }}>
-          Sign Up
-        </Button>
-      </ModalBody>
-    </Modal>
-  );
-};
+class PremiumModal extends Component {
+  state = {
+    name: '',
+    email: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <Modal centered isOpen={this.props.isOpen} toggle={this.props.toggle}>
+        {/* <ModalBody>Sign up</ModalBody> */}
+        <ModalHeader toggle={this.props.toggle}>
+          Give us your email address, and we'll notify you when our premium
+          service is available.
+        </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Input
+                type="name"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="name@example.com"
+              />
+            </FormGroup>
+          </Form>
+          <Button size="lg" block color="info" style={{ marginTop: '20px' }}>
+            Sign Up
+          </Button>
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
 
 export default PremiumModal;
