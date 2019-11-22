c3ab607458d650af3e72835c8519e0350f4d0ae5
DirupT <jjaguirre73@yahoo.com>
2019-11-22 16:38:25 -0600

    updated PDFModal.js to use hooks

--- client/src/components/Documents/PDFModal.js
+++ client/src/components/Documents/PDFModal.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PDFObject from 'pdfobject';
 import { Modal } from 'reactstrap';
 import { ModalPDF } from './styles/DocumentModalStyles';
 
-class PDFModal extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (prevProps.isOpen !== this.props.isOpen && this.props.isOpen) {
-      const data = `data:application/pdf;base64,${this.props.document}`;
-      PDFObject.embed(data, `#pdf_subject${this.props.doc.id}`);
-    }
-  }
+const PDFModal = props => {
+  useEffect(() => {
+    const data = `data:application/pdf;base64,${props.document}`;
+    PDFObject.embed(data, `#pdf_subject${props.doc.id}`);
+  }, [props.isOpen]);
 
-  render() {
-    const { doc, toggle, isOpen, document } = this.props;
-    if (!document || !isOpen) return null;
+  const { doc, toggle, isOpen, document } = props;
+  if (!document || !isOpen) return null;
 
-    return (
-      <Modal isOpen={isOpen} toggle={toggle} size="lg">
-        <ModalPDF id={`pdf_subject${doc.id}`} />
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal isOpen={isOpen} toggle={toggle} size="lg">
+      <ModalPDF id={`pdf_subject${doc.id}`} />
+    </Modal>
+  );
+};
 
 export default PDFModal;
